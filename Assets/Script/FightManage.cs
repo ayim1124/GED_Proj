58e0d82ee5b77cc3c1f85d3d7abdafd4df6b6894
ayim <ayim1115@gmail.com>
2015-04-18 10:52:46 +0900

    Init

---
+++ Assets/Script/FightManage.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FightManage : MonoBehaviour {
+
+    public static float _Gev = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
